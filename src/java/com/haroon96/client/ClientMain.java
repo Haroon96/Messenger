e74b46244a9990d8de3e48f40b0ed87eda4ecd90
Muhammad Haroon <m_haroon96@hotmail.com>
2018-05-20 18:34:07 +0500

    Refactored drivers

---
+++ src/java/com/haroon96/client/ClientMain.java
@@ -0,0 +1,9 @@
+package com.haroon96.client;
+
+import com.haroon96.client.gui.LoginFrame;
+
+public class ClientMain {
+	public static void main(String[] args) {
+		new LoginFrame().setVisible(true);
+	}
+}
